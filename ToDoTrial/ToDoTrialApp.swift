ade099c6bd45b4793b77aeded57888fe1d1d642e
Maddie Dew <maddiedew12@gmail.com>
2023-06-27 10:18:55 -0600

    Initial Commit

---
+++ ToDoTrial/ToDoTrialApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoTrialApp.swift
+//  ToDoTrial
+//
+//  Created by Scholar on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoTrialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
