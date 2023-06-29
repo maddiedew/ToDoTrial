addb46b85acbfe3044bb5a7c1f1738cf36af5cba
Maddie Dew <maddiedew12@gmail.com>
2023-06-29 09:00:21 -0600

    complete

--- ToDoTrial/ContentView.swift
+++ ToDoTrial/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var toDoItems : [ToDoItem] = []
+    @State private var showNewTask = false
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                
+                Text("To Do List")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding()
+                
+                Button(action: {
+                    self.showNewTask = true
+                }) {
+                    Text("+")
+            }
+           
+            }
+            Spacer()
         }
-        .padding()
+        
+        List {
+            ForEach (toDoItems) { toDoItem in
+                if toDoItem.isImportant == true {
+                    Text("‼️ " + toDoItem.title)
+                } else {
+                    Text(toDoItem.title)
+                }
+                
+            }
+        }
+        .listStyle(.plain)
+
+        if showNewTask {
+            NewToDoView(title: "", isImportant : false, toDoItems: $toDoItems, showNewTask: $showNewTask)
+        }
     }
 }
 
